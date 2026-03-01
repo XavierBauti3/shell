42ec2545ab335f038162d2906a66acd6b79ad787
Xavier Bautista <nelson.bautistasalazar@mail.utoronto.ca>
2026-03-01 16:32:35 -0500

    This iteration includes some changes to copy files instead of moving the files

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -42,11 +42,11 @@ mkdir ./data/processed
 mkdir ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-mv ./data/raw/server_log* ./data/processed/server_logs/
+cp ./data/raw/server_log* ./data/processed/server_logs/
 
 # 6. Repeat the above step for user logs and event logs
-mv ./data/raw/user_log* ./data/processed/user_logs/
-mv ./data/raw/event_log* ./data/processed/event_logs/
+cp ./data/raw/user_log* ./data/processed/user_logs/
+cp ./data/raw/event_log* ./data/processed/event_logs/
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
 rm ./data/raw/*ipaddr* ./data/processed/user_logs/*ipaddr*
